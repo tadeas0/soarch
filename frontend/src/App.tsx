9038af3235bfb8ee4dee49b55448f908ac37b157
Tadeas Pala <tadapala@seznam.cz>
2021-10-15 16:01:24 +0200

    prepare synthesizer class

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import PianoRoll from "./components/pianoRoll";
+import { Synthesizer } from "./synthesizer";
 
 function App() {
+    const playSequence = () => {
+        const a = new Synthesizer();
+        a.init();
+        console.log("play");
+    };
+
     return (
         <div className="App">
+            <button onClick={playSequence}>Play</button>
             <PianoRoll />
         </div>
     );
