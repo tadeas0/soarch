0c834c9b358db830f0f7348561c262191b3ff069
Tadeas Pala <tadapala@seznam.cz>
2021-10-17 15:12:06 +0200

    create song repository

--- backend/src/app/search_engine/repository.py
+++ backend/src/app/search_engine/repository.py
@@ -1,6 +1,21 @@
-# class SongRepository:
-#     def load_from_file():
-#         pass
+from app.search_engine.song import Song
+from app.search_engine.parser import MidiParser
+from miditoolkit.midi import MidiFile
+import glob
+import os
+from typing import List, Set
 
-#     def get_all():
-#         pass
+
+class SongRepository:
+    def __init__(self) -> None:
+        self.__songs: List[Song] = []
+
+    def load_song(self, file_path: str):
+        self.__songs.append(MidiParser.parse(MidiFile(file_path)))
+
+    def load_directory(self, directory: str):
+        for i in glob.glob(os.path.join(directory, "*.mid")):
+            self.__songs.append(MidiParser.parse(MidiFile(i)))
+
+    def get_all(self):
+        return self.__songs
