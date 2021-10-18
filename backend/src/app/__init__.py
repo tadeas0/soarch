df2d27d406f80fa32f5f98ba75afbb646d62cc88
Tadeas Pala <tadapala@seznam.cz>
2021-10-18 21:54:19 +0200

    create app factory

--- backend/src/app/__init__.py
+++ backend/src/app/__init__.py
@@ -1,12 +1,5 @@
 from flask import Flask
 import config
-
-
-app = Flask(__name__)
-app.config.from_json("../secrets.json")
-app.url_map.strict_slashes = False
-
-
 from app.search_engine.repository import SongRepository
 from app.search_engine.search_engine import SearchEngine
 from app.search_engine.similarity_strategy import DTWStrategy
@@ -15,21 +8,30 @@ from app.search_engine.melody_extraction_strategy import TopNoteStrategy
 from app.search_engine.standardization_strategy import RelativeIntervalStrategy
 
 repository = SongRepository()
-repository.load_directory(config.PROCESSED_MIDI)
 engine = SearchEngine(
     repository, TopNoteStrategy(), RelativeIntervalStrategy(), DTWStrategy()
 )
 
-from app.midi.controller import midi_bp
-from app.health_check.controller import health_check_bp
 
-app.register_blueprint(midi_bp)
-app.register_blueprint(health_check_bp)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_json("../secrets.json")
+    app.url_map.strict_slashes = False
 
-from app.scripts.parse_to_db import parse_to_db
-from app.scripts.scrape_robs_library import scrape_robs_library
-from app.scripts.scrape_freemidi import scrape_freemidi
+    repository.load_directory(config.PROCESSED_MIDI)
 
-app.cli.add_command(parse_to_db)
-app.cli.add_command(scrape_freemidi)
-app.cli.add_command(scrape_robs_library)
+    from app.midi.controller import midi_bp
+    from app.health_check.controller import health_check_bp
+
+    app.register_blueprint(midi_bp)
+    app.register_blueprint(health_check_bp)
+
+    from app.scripts.parse_to_db import parse_to_db
+    from app.scripts.scrape_robs_library import scrape_robs_library
+    from app.scripts.scrape_freemidi import scrape_freemidi
+
+    app.cli.add_command(parse_to_db)
+    app.cli.add_command(scrape_freemidi)
+    app.cli.add_command(scrape_robs_library)
+
+    return app
--- backend/src/run.py
+++ backend/src/run.py
@@ -1,4 +1,4 @@
-from app import app
+from app import create_app
 
 
-app.run(host="0.0.0.0", port=8080, debug=True)
+create_app().run(host="0.0.0.0", port=8080, debug=True)
