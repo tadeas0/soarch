4faf30469b55106f7d9fef48a2117f659ed81671
Tadeas Pala <tadapala@seznam.cz>
2021-10-22 00:41:03 +0200

    implement redis file caching for google cloud storage

--- backend/src/app/midi/filestorage.py
+++ backend/src/app/midi/filestorage.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
-from typing import List, IO
+from typing import List, IO, Optional
 from google.cloud import storage
+import redis
 import io
 import os
 
@@ -56,7 +57,18 @@ class LocalFileStorage(FileStorage):
 
 
 class GoogleCloudFileStorage(FileStorage):
-    def __init__(self, credentials_json: dict, bucket_name: str) -> None:
+    def __init__(
+        self,
+        credentials_json: dict,
+        bucket_name: str,
+        redis_cache_host: str = None,
+        redis_cache_port: int = None,
+    ) -> None:
+        self.__redis_cache: Optional[redis.Redis] = None
+        if redis_cache_host and redis_cache_port:
+            self.__redis_cache = redis.Redis(
+                host=redis_cache_host, port=redis_cache_port
+            )
         self.__storage_client = storage.Client.from_service_account_info(
             credentials_json
         )
@@ -64,6 +76,12 @@ class GoogleCloudFileStorage(FileStorage):
         self.__bucket = self.__storage_client.bucket(bucket_name)
 
     def open(self, key: str, mode: str) -> IO:
+        if self.__redis_cache:
+            return self.__open_cache(key, mode)
+        else:
+            return self.__open_no_cache(key, mode)
+
+    def __open_no_cache(self, key: str, mode: str) -> IO:
         blob = self.__bucket.get_blob(key)
         if blob is None and mode not in ["w", "wb"]:
             raise FileNotFoundError()
@@ -74,7 +92,36 @@ class GoogleCloudFileStorage(FileStorage):
         elif mode == "r":
             return io.TextIOWrapper(blob.download_as_text())
         else:
-            raise ValueError(f"invalid mode {mode}")
+            raise ValueError(f"invalid mode: {mode}")
+
+    def __open_cache(self, key: str, mode: str) -> IO:
+        if self.__redis_cache:
+            if mode in ["r", "rb"]:
+                res = self.__redis_cache.get(key)
+                if res and mode == "r":
+                    return io.TextIOWrapper(io.BytesIO(res))
+                elif res and mode == "rb":
+                    return io.BytesIO(res)
+                elif not res and mode == "r":
+                    blob = self.__bucket.get_blob(key)
+                    if not blob:
+                        raise FileNotFoundError()
+                    content = blob.download_as_string()
+                    self.__redis_cache.set(key, content)
+                    return io.TextIOWrapper(io.BytesIO(content))
+                elif not res and mode == "rb":
+                    blob = self.__bucket.get_blob(key)
+                    if not blob:
+                        raise FileNotFoundError()
+                    content = blob.download_as_bytes()
+                    self.__redis_cache.set(key, content)
+                    return io.BytesIO(content)
+            elif mode in ["w", "wb"]:
+                self.__redis_cache.delete(key)
+                return self.__bucket.blob(key).open(mode)
+            raise ValueError(f"invalid mode: {mode}")
+        else:
+            raise ValueError("redis cache not defined")
 
     def list(self) -> List[str]:
         return list(
