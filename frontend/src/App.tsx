16787882ce61a4a45b90c38245b323b2a1182dc5
Tadeas Pala <tadapala@seznam.cz>
2021-10-23 12:15:34 +0200

    parse search results

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { useState } from "react";
 import * as Tone from "tone";
 import "./App.css";
 import PianoRoll from "./components/pianoRoll";
 import { Note } from "./sequencer";
 import { API } from "./services/api";
 
+interface SearchResult {
+    artist: string;
+    name: string;
+    notes: Note[];
+}
+
 function App() {
+    const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
     const handleSubmit = (notes: Note[], gridLength: number) => {
         API.postNotes({
             gridLength: gridLength,
@@ -16,7 +24,22 @@ function App() {
                     time: n.time,
                 };
             }),
-        }).then((res) => console.log(res.data));
+        }).then((res) => {
+            const result = res.data.tracks.map<SearchResult>((track) => {
+                return {
+                    artist: track.artist,
+                    name: track.name,
+                    notes: track.notes.map<Note>((n) => {
+                        return {
+                            time: Tone.Time(n.time).toBarsBeatsSixteenths(),
+                            pitch: Tone.Frequency(n.pitch, "midi").toNote(),
+                            length: Tone.Time(n.length).toBarsBeatsSixteenths(),
+                        };
+                    }),
+                };
+            });
+            setSearchResults(result);
+        });
     };
 
     return (
---
+++ frontend/src/components/result.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { FunctionComponent } from "react";
+
+interface SearchResultProps {}
+
+const SearchResult: FunctionComponent<SearchResultProps> = () => {
+    return <div>Result</div>;
+};
+
+export default SearchResult;
