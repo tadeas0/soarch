7ee44f07a7d25b25fa21d692ca1bb1bed4fac6ad
Tadeas Pala <tadapala@seznam.cz>
2021-12-07 15:37:25 +0100

    implement DTW

--- backend/src/app/search_engine/similarity_strategy.py
+++ backend/src/app/search_engine/similarity_strategy.py
@@ -26,16 +26,16 @@ class SimilarityStrategy(ABC):
         pass
 
 
-class EMDStrategy(SimilarityStrategy):
+class EMDStrategySP(SimilarityStrategy):
     highest_first = False
 
     @property
     def name(self) -> str:
-        return "Earth mover's distance"
+        return "Earth mover's distance (SciPy library)"
 
     @property
     def shortcut(self) -> str:
-        return "emd"
+        return "emdsp"
 
     def compare(
             self, line1: npt.NDArray[np.int64], line2: npt.NDArray[np.int64]
@@ -74,21 +74,21 @@ class LCSStrategy(SimilarityStrategy):
         return self.__measure_aux(line1, line2)
 
 
-class DTWStrategy(SimilarityStrategy):
+class FDTWStrategyLib(SimilarityStrategy):
     highest_first = False
 
     @property
     def name(self) -> str:
-        return "Dynamic time warping"
+        return "Fast dynamic time warping (FastDTW library)"
 
     @property
     def shortcut(self) -> str:
-        return "dtw"
+        return "fdtwl"
 
     def compare(
         self, line1: npt.NDArray[np.int64], line2: npt.NDArray[np.int64]
     ) -> float:
-        dist, _ = fastdtw(line1, line2)
+        dist, _ = fastdtw(line1, line2, min(len(line1), len(line2)))
         return dist
 
 
@@ -109,3 +109,68 @@ class LocalAlignmentStrategy(SimilarityStrategy):
         tl1 = "".join(chr(int(i) + 128) for i in line1)
         tl2 = "".join(chr(int(i) + 128) for i in line2)
         return pairwise2.align.localms(tl1, tl2, 1, -1, -2, -2, score_only=True)
+
+
+class DTWStrategy(SimilarityStrategy):
+    highest_first = False
+
+    @property
+    def name(self) -> str:
+        return "Dynamic time warping"
+
+    @property
+    def shortcut(self) -> str:
+        return "dtw"
+
+    def compare(
+        self, line1: npt.NDArray[np.int64], line2: npt.NDArray[np.int64]
+    ) -> float:
+        len1 = len(line1)
+        len2 = len(line2)
+        dtw = np.zeros((len1 + 1, len2 + 1))
+        for i in range(len1 + 1):
+            for j in range(len2 + 1):
+                dtw[i][j] = np.inf
+        dtw[0, 0] = 0
+        for i in range(1, len1 + 1):
+            for j in range(1, len2 + 1):
+                cost = abs(line1[i - 1] - line2[j - 1])
+                last_min = min(dtw[i - 1][j], dtw[i][j - 1], dtw[i - 1][j - 1])
+                dtw[i][j] = cost + last_min
+        return dtw[-1][-1]
+
+
+class DTWWinStrategy(SimilarityStrategy):
+    highest_first = False
+    window = 3
+
+    @property
+    def name(self) -> str:
+        return "Dynamic time warping with window constraint"
+
+    @property
+    def shortcut(self) -> str:
+        return "dtwwin"
+
+    def compare(
+        self, line1: npt.NDArray[np.int64], line2: npt.NDArray[np.int64]
+    ) -> float:
+        n, m = len(line1), len(line2)
+        w = np.max([self.window, abs(n - m)])
+        dtw_matrix = np.zeros((n + 1, m + 1))
+
+        for i in range(n + 1):
+            for j in range(m + 1):
+                dtw_matrix[i][j] = np.inf
+        dtw_matrix[0, 0] = 0
+
+        for i in range(1, n + 1):
+            for j in range(np.max([1][i - w]), np.min([m][i + w]) + 1):
+                dtw_matrix[i][j] = 0
+
+        for i in range(1, n + 1):
+            for j in range(np.max([1][i - w]), np.min([m][i + w]) + 1):
+                cost = abs(line1[i - 1] - line2[j - 1])
+                last_min = np.min([dtw_matrix[i - 1][j], dtw_matrix[i][j - 1], dtw_matrix[i - 1][j - 1]])
+                dtw_matrix[i][j] = cost + last_min
+        return dtw_matrix[-1][-1]
