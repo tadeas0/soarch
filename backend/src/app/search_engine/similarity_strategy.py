2445484e659afca90f1a743bba52a7f7c15a5c06
Tadeas Pala <tadapala@seznam.cz>
2021-12-07 16:05:22 +0100

    fix flake8 errors

--- backend/src/app/search_engine/similarity_strategy.py
+++ backend/src/app/search_engine/similarity_strategy.py
@@ -157,20 +157,20 @@ class DTWWinStrategy(SimilarityStrategy):
     ) -> float:
         n, m = len(line1), len(line2)
         w = np.max([self.window, abs(n - m)])
-        dtw_matrix = np.zeros((n + 1, m + 1))
+        dtw = np.zeros((n + 1, m + 1))
 
         for i in range(n + 1):
             for j in range(m + 1):
-                dtw_matrix[i][j] = np.inf
-        dtw_matrix[0, 0] = 0
+                dtw[i][j] = np.inf
+        dtw[0, 0] = 0
 
         for i in range(1, n + 1):
             for j in range(np.max([1][i - w]), np.min([m][i + w]) + 1):
-                dtw_matrix[i][j] = 0
+                dtw[i][j] = 0
 
         for i in range(1, n + 1):
             for j in range(np.max([1][i - w]), np.min([m][i + w]) + 1):
                 cost = abs(line1[i - 1] - line2[j - 1])
-                last_min = np.min([dtw_matrix[i - 1][j], dtw_matrix[i][j - 1], dtw_matrix[i - 1][j - 1]])
-                dtw_matrix[i][j] = cost + last_min
-        return dtw_matrix[-1][-1]
+                last_min = min(dtw[i - 1][j], dtw[i][j - 1], dtw[i - 1][j - 1])
+                dtw[i][j] = cost + last_min
+        return dtw[-1][-1]
