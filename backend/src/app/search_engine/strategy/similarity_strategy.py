15f569980a589010cef1256468b1c023b478d074
Tadeas Pala <tadapala@seznam.cz>
2021-12-10 18:09:26 +0100

    implement local alignment similarity strategy

--- backend/src/app/search_engine/strategy/similarity_strategy.py
+++ backend/src/app/search_engine/strategy/similarity_strategy.py
@@ -151,6 +151,41 @@ class LocalAlignmentStrategy(SimilarityStrategy):
     def shortcut(self) -> str:
         return "lca"
 
+    def __clamp_num(self, a: int) -> int:
+        if a < 0:
+            return 0
+        return a
+
+    def compare(
+        self, line1: npt.NDArray[np.int64], line2: npt.NDArray[np.int64]
+    ) -> float:
+        match = 1
+        mismatch = -1
+        gap = -2
+        score_mat = np.zeros((len(line1) + 1, len(line2) + 1))
+        for i in range(1, len(line1) + 1):
+            for j in range(1, len(line2) + 1):
+                ln = self.__clamp_num(score_mat[i][j - 1] + gap)
+                un = self.__clamp_num(score_mat[i - 1][j] + gap)
+                if line1[i - 1] == line2[j - 1]:
+                    dn = self.__clamp_num(score_mat[i - 1][j - 1] + match)
+                else:
+                    dn = self.__clamp_num(score_mat[i - 1][j - 1] + mismatch)
+                score_mat[i][j] = max(ln, un, dn)
+        return np.max(score_mat)
+
+
+class LocalAlignmentStrategyLib(SimilarityStrategy):
+    highest_first = True
+
+    @property
+    def name(self) -> str:
+        return "Local alignment (Biopython library)"
+
+    @property
+    def shortcut(self) -> str:
+        return "lcabp"
+
     def compare(
         self, line1: npt.NDArray[np.int64], line2: npt.NDArray[np.int64]
     ) -> float:
