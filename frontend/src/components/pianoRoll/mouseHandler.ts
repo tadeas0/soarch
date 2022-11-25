e6423d3c898ed5d88e496d711f5148631b535ebc
Tadeas Pala <tadapala@seznam.cz>
2022-11-25 16:57:15 +0100

    make note handle take up only last part of the square

--- frontend/src/components/pianoRoll/mouseHandler.ts
+++ frontend/src/components/pianoRoll/mouseHandler.ts
@@ -1,6 +1,7 @@
 import { useEffect, useRef } from "react";
 import * as Tone from "tone";
 import {
+    PIANO_ROLL_HANDLE_PART,
     PIANO_ROLL_HEADER_SIZE,
     PIANO_ROLL_NOTE_HEIGHT,
     PIANO_ROLL_NOTE_WIDTH,
@@ -43,7 +44,7 @@ export class MouseHandler {
         this._selectedNote = null;
     }
 
-    private getRollCoordsFromMouseCoords(coords: MouseCoords): RollCoordinates {
+    public getRollCoordsFromMouseCoords(coords: MouseCoords): RollCoordinates {
         const { offsetX, offsetY } = coords;
         return {
             row: Math.floor(
@@ -65,14 +66,13 @@ export class MouseHandler {
         this._playbackEnabled = value;
     }
 
-    public getNotesAt(coords: RollCoordinates): Note[] {
+    public getNotesAt(coords: MouseCoords): Note[] {
+        const c = this.getRollCoordsFromMouseCoords(coords);
         return this.getNotes().filter((n) => {
             const s = Sequencer.getStartCoords(n, this._gridParams);
             const e = Sequencer.getEndCoords(n, this._gridParams);
             return (
-                s.row === coords.row &&
-                s.column <= coords.column &&
-                e.column >= coords.column
+                s.row === c.row && s.column <= c.column && e.column >= c.column
             );
         });
     }
@@ -84,20 +84,26 @@ export class MouseHandler {
      * @param notes notes for which the coordinates should be checked
      * @returns Note | null
      */
-    public noteHandle(coords: RollCoordinates): Note | null {
+    public noteHandle(coords: MouseCoords): Note | null {
+        const { offsetX } = coords;
+        const isHandle =
+            offsetX % PIANO_ROLL_NOTE_WIDTH >
+            PIANO_ROLL_NOTE_WIDTH * (1 - PIANO_ROLL_HANDLE_PART);
+        if (!isHandle) return null;
         const clickedNotes = this.getNotesAt(coords);
         if (clickedNotes.length <= 0) {
             return null;
         }
         const topNote = clickedNotes[clickedNotes.length - 1];
         const endCoords = Sequencer.getEndCoords(topNote, this._gridParams);
-        if (this.cmpCoords(coords, endCoords)) {
+        const rollCoords = this.getRollCoordsFromMouseCoords(coords);
+        if (this.cmpRollCoords(rollCoords, endCoords)) {
             return topNote;
         }
         return null;
     }
 
-    private cmpCoords(
+    private cmpRollCoords(
         coords1: RollCoordinates,
         coords2: RollCoordinates
     ): boolean {
@@ -130,25 +136,23 @@ export class MouseHandler {
     }
 
     public onLeftClick(coords: MouseCoords) {
-        this.state.handleLeftClick(this.getRollCoordsFromMouseCoords(coords));
+        this.state.handleLeftClick(coords);
     }
 
     public onRightClick(coords: MouseCoords) {
-        this.state.handleRightClick(this.getRollCoordsFromMouseCoords(coords));
+        this.state.handleRightClick(coords);
     }
 
     public onMouseMove(coords: MouseCoords) {
-        this.state.handleMouseMove(this.getRollCoordsFromMouseCoords(coords));
+        this.state.handleMouseMove(coords);
     }
 
     public onLeftRelease(coords: MouseCoords) {
-        this.state.handleLeftRelease(this.getRollCoordsFromMouseCoords(coords));
+        this.state.handleLeftRelease(coords);
     }
 
     public onRightRelease(coords: MouseCoords) {
-        this.state.handleRightRelease(
-            this.getRollCoordsFromMouseCoords(coords)
-        );
+        this.state.handleRightRelease(coords);
     }
 
     public pressNote(note: Tone.Unit.Note) {
