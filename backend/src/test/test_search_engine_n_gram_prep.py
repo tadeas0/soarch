44fa7a15526d22ad231be661e2c2420a9f222847
Tadeas Pala <tadapala@seznam.cz>
2023-02-23 13:23:57 +0100

    add test for search engine with n gram filter

--- backend/src/test/test_search_engine_n_gram_prep.py
+++ backend/src/test/test_search_engine_n_gram_prep.py
@@ -1,12 +1,12 @@
 from app.util.song import Note, Song, Track
-from app.search_engine.search_engine import SearchEngine
 from app.search_engine.strategy.melody_extraction_strategy import TopNoteStrategy
 from app.search_engine.strategy.standardization_strategy import RelativeIntervalStrategy
 from app.search_engine.strategy.similarity_strategy import LCSStrategy
 from app.search_engine.strategy.segmentation_strategy import OneSegmentStrategy
 import pytest
 from app.search_engine.preprocessor import Preprocessor
-from test.mocks.mock_repository import MockRepository
+from app.search_engine.search_engine_n_gram_prep import SearchEngineNGramPrep
+from test.test_search_engine import MockRepository
 
 
 def assert_result(result, expected_len):
@@ -24,12 +24,14 @@ async def test_find_similar_async():
     prep = Preprocessor(
         TopNoteStrategy(), RelativeIntervalStrategy(), OneSegmentStrategy()
     )
-    search_engine = SearchEngine(
+    search_engine = SearchEngineNGramPrep(
         repository,
         prep,
         LCSStrategy(),
     )
-    query = Track([Note(0, 10, 32), Note(30, 10, 32)], 150)
+    query = Track(
+        [Note(0, 10, 32), Note(30, 10, 32), Note(40, 10, 32), Note(50, 10, 32)], 150
+    )
     result1 = await search_engine.find_similar_async(2, query)
     result2 = await search_engine.find_similar_async(3, query)
     result3 = await search_engine.find_similar_async(5, query)
