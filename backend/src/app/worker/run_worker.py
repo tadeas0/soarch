799ea2a752941549b4cc016624676a1a5301b674
Tadeas Pala <tadapala@seznam.cz>
2023-03-06 14:14:07 +0100

    make broker setup callable

--- backend/src/app/worker/run_worker.py
+++ backend/src/app/worker/run_worker.py
@@ -9,27 +9,21 @@ from app.repository.mongo_song_repository import MongoSongRepository
 from dramatiq.results.backends import RedisBackend
 
 
-class InitRepoMiddleware(dramatiq.Middleware):
-    def before_declare_actor(self, broker, actor):
-        logger = setup_logging()
-        logger.info("Logging initialized")
+def setup_broker():
+    worker_backend = RedisBackend(url=config.REDIS_QUEUE_URL)
+    broker = RedisBroker(url=config.REDIS_QUEUE_URL)
+    broker.add_middleware(Results(backend=worker_backend))
+    logger = setup_logging()
+    logger.info("Logging initialized")
 
-        if type(file_storage) == GoogleCloudFileStorage:
-            logger.info("Using google cloud file storage")
-        else:
-            logger.info("Using local file storage")
-        if type(repository) == MongoSongRepository:
-            logger.info("Using MongoDB repository")
-        else:
-            logger.info("Using file repository")
-        file_storage.initialize()
-        logger.info("File storage initialized")
-
-        return super().before_declare_actor(broker, actor)
-
-
-worker_backend = RedisBackend(url=config.REDIS_QUEUE_URL)
-broker = RedisBroker(url=config.REDIS_QUEUE_URL)
-broker.add_middleware(Results(backend=worker_backend))
-broker.add_middleware(InitRepoMiddleware())
-dramatiq.set_broker(broker)
+    if type(file_storage) == GoogleCloudFileStorage:
+        logger.info("Using google cloud file storage")
+    else:
+        logger.info("Using local file storage")
+    if type(repository) == MongoSongRepository:
+        logger.info("Using MongoDB repository")
+    else:
+        logger.info("Using file repository")
+    file_storage.initialize()
+    logger.info("File storage initialized")
+    dramatiq.set_broker(broker)
