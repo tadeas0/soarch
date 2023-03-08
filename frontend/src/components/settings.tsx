7658bea596b5495fa315addf52ac68490999d583
Tadeas Pala <tadapala@seznam.cz>
2023-03-08 14:45:14 +0100

    add settings for note playback and count in

--- frontend/src/components/settings.tsx
+++ frontend/src/components/settings.tsx
@@ -8,13 +8,22 @@ import { MAX_VOLUME_DB, MIN_VOLUME_DB } from "../constants";
 import useClickawayListener from "../hooks/useClickawayListener";
 import Tooltip from "./basic/tooltip";
 import { AiOutlineInfoCircle } from "react-icons/ai";
+import Checkbox from "./basic/checkbox";
 
 interface SettingsProps {}
 
 const Settings: FunctionComponent<SettingsProps> = () => {
     const [open, setOpen] = useState(false);
-    const { useFasterSearch, setUseFasterSearch, volume, setVolume } =
-        useSettingsStore();
+    const {
+        useFasterSearch,
+        setUseFasterSearch,
+        volume,
+        setVolume,
+        notePlayback,
+        useCountIn,
+        setUseCountIn,
+        setNotePlayback,
+    } = useSettingsStore();
     const divRef = useRef<HTMLDivElement>(null);
     useClickawayListener(divRef, () => {
         setOpen(false);
@@ -42,24 +51,36 @@ const Settings: FunctionComponent<SettingsProps> = () => {
             {open && (
                 <div className="rounded-r-md border-2 border-l-0 border-black bg-light-primary text-white">
                     <ul>
-                        <li className="group/fastersearch">
-                            <label className="flex flex-row items-center rounded-tr-md p-2 group-hover/fastersearch:bg-medium-primary group-hover/fastersearch:text-black">
-                                <input
-                                    id="fasterSearch"
-                                    type="checkbox"
-                                    checked={useFasterSearch}
-                                    onChange={() =>
-                                        setUseFasterSearch(!useFasterSearch)
-                                    }
-                                    className="mr-4 rounded bg-white text-medium-primary focus:ring-0 focus:ring-offset-0 group-hover/fastersearch:text-dark-primary"
-                                />
+                        <li>
+                            <Checkbox
+                                value={useFasterSearch}
+                                onChange={() =>
+                                    setUseFasterSearch(!useFasterSearch)
+                                }
+                            >
                                 Use faster search
                                 <span className="ml-4">
                                     <Tooltip text="Sacrifice some accuracy in order to provide faster results.">
                                         <AiOutlineInfoCircle />
                                     </Tooltip>
                                 </span>
-                            </label>
+                            </Checkbox>
+                        </li>
+                        <li>
+                            <Checkbox
+                                value={notePlayback}
+                                onChange={() => setNotePlayback(!notePlayback)}
+                            >
+                                Note playback
+                            </Checkbox>
+                        </li>
+                        <li>
+                            <Checkbox
+                                value={useCountIn}
+                                onChange={() => setUseCountIn(!useCountIn)}
+                            >
+                                Use count-in
+                            </Checkbox>
                         </li>
                         <li className="rounded-l-md p-2">
                             <label htmlFor="volume">
@@ -68,7 +89,7 @@ const Settings: FunctionComponent<SettingsProps> = () => {
                             <input
                                 id="volume"
                                 type="range"
-                                className="accent-medium-primary outline-none focus:ring-0 focus:ring-offset-0"
+                                className="w-full accent-medium-primary outline-none focus:ring-0 focus:ring-offset-0"
                                 min={0}
                                 max={1}
                                 step={0.1}
--- frontend/src/stores/settingsStore.ts
+++ frontend/src/stores/settingsStore.ts
@@ -4,8 +4,12 @@ import { persist } from "zustand/middleware";
 export interface SettingsState {
     volume: number;
     useFasterSearch: boolean;
+    notePlayback: boolean;
+    useCountIn: boolean;
+    setUseCountIn: (val: boolean) => void;
     setVolume: (val: number) => void;
     setUseFasterSearch: (val: boolean) => void;
+    setNotePlayback: (val: boolean) => void;
 }
 
 export const useSettingsStore = create<SettingsState>()(
@@ -13,9 +17,14 @@ export const useSettingsStore = create<SettingsState>()(
         (set) => ({
             volume: 0.5,
             useFasterSearch: true,
+            notePlayback: true,
+            useCountIn: true,
+            setNotePlayback: (val: boolean) =>
+                set(() => ({ notePlayback: val })),
             setVolume: (val: number) => set(() => ({ volume: val })),
             setUseFasterSearch: (val: boolean) =>
                 set(() => ({ useFasterSearch: val })),
+            setUseCountIn: (val: boolean) => set(() => ({ useCountIn: val })),
         }),
         {
             name: "settings",
